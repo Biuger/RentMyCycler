48e27b9efb6e0ff9264f3a6b56ce16c099bdbb87
Biuger <biuger277@gmail.com>
2024-05-25 23:29:25 -0600

    start with data 2

--- RentMyCycler.Website/Services/UserService.cs
+++ RentMyCycler.Website/Services/UserService.cs
@@ -1,4 +1,4 @@
-
+using Newtonsoft.Json;
 using RentMyCycler.Api.DTO;
 using RentMyCycler.Core.http;
 using RentMyCycler.Website.Services.Interfaces;
@@ -7,5 +7,75 @@ namespace RentMyCycler.Website.Services;
 
 public class UserService: IUserService
 {
-    
+    private readonly string _baseURL = "http://localhost:5080/";
+    private readonly string _endpoint = "api/users";
+
+    public UserService()
+    {
+        
+    }
+
+    public async Task<Response<List<UserDto>>> GetAllAsync()
+    {
+        var url = $"{_baseURL}{_endpoint}";
+        var client = new HttpClient();
+        var res = await client.GetAsync(url);
+        var json = await res.Content.ReadAsStringAsync();
+
+        var response = JsonConvert.DeserializeObject<Response<List<UserDto>>>(json);
+
+        return response;
+    }
+
+    public async Task<Response<UserDto>> GetById(int id)
+    {
+        var url = $"{_baseURL}{_endpoint}/{id}";
+
+        var client = new HttpClient();
+        var res = await client.GetAsync(url);
+        var json = await res.Content.ReadAsStringAsync();
+        var response = JsonConvert.DeserializeObject<Response<UserDto>>(json);
+
+        return response;
+    }
+
+    public async Task<Response<UserDto>> SaveAsync(UserDto UserDto)
+    {
+        var url = $"{_baseURL}{_endpoint}";
+        var jsonRequest = JsonConvert.SerializeObject(UserDto);
+        var content = new StringContent(jsonRequest, System.Text.Encoding.UTF8, "application/json");
+        var client = new HttpClient();
+        var res = await client.PostAsync(url, content);
+        var json = await res.Content.ReadAsStringAsync();
+
+        var response = JsonConvert.DeserializeObject<Response<UserDto>>(json);
+
+        return response;
+    }
+
+    public async Task<Response<UserDto>> UpdateAsync(UserDto UserDto)
+    {
+        var url = $"{_baseURL}{_endpoint}";
+        var jsonRequest = JsonConvert.SerializeObject(UserDto);
+        var content = new StringContent(jsonRequest, System.Text.Encoding.UTF8, "application/json");
+        var client = new HttpClient();
+        var res = await client.PutAsync(url, content);
+        var json = await res.Content.ReadAsStringAsync();
+
+        var response = JsonConvert.DeserializeObject<Response<UserDto>>(json);
+
+        return response;
+    }
+
+    public async Task<Response<bool>> DeleteAsync(int id)
+    {
+        var url = $"{_baseURL}{_endpoint}/{id}";
+        var client = new HttpClient();
+        var res = await client.DeleteAsync(url);
+        var json = await res.Content.ReadAsStringAsync();
+
+        var response = JsonConvert.DeserializeObject<Response<bool>>(json);
+
+        return response;
+    }
 }
